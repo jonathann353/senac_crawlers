14134ba90086c732816a4621d5e21eb0890d32af
Lucas Gades <70823238+Lucas-Gades@users.noreply.github.com>
2024-10-28 19:18:37 -0300

    Update settings.py

--- api-senac-crawlers/api_root/settings.py
+++ api-senac-crawlers/api_root/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
